d2f1c168c9898d3917108cbac1ba4c0c8d3e3410
xavsln <19362582+xavsln@users.noreply.github.com>
2022-05-25 16:38:45 +0700

    Create a list of Pokemons as Objects into an Array

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Create a new Array variable
+let pokemonList = [];
+
+// Create various pokemons
+let pokemon1 = {
+  name: 'Bulbasaur',
+  types: ['grass', 'poison'],
+  height: 0.7
+};
+
+let pokemon2 = {
+  name: 'Charmander',
+  types: ['fire'],
+  height: 0.6
+};
+
+let pokemon3 = {
+  name: 'Squirtle',
+  types: ['water'],
+  height: 0.5
+};
+
+let pokemon4 = {
+  name: 'Caterpie',
+  types: ['bug'],
+  height: 0.3
+};
+
+// Assign pokemon objects to our pokemonList
+pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4]
