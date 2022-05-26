185c1fac832ebb27f75a9380a36109e694a0535e
xavsln <19362582+xavsln@users.noreply.github.com>
2022-05-26 17:50:48 +0700

    Create Pokemon Objects directly inside the Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
-// Create a new Array variable
-let pokemonList = [];
+// Create a new Array of Pokemon
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    types: ['grass', 'poison'],
+    height: 0.7
+  },
 
-// Create various pokemons
-let pokemon1 = {
-  name: 'Bulbasaur',
-  types: ['grass', 'poison'],
-  height: 0.7
-};
+  {
+    name: 'Charmander',
+    types: ['fire'],
+    height: 0.6
+  },
 
-let pokemon2 = {
-  name: 'Charmander',
-  types: ['fire'],
-  height: 0.6
-};
+  {
+    name: 'Squirtle',
+    types: ['water'],
+    height: 0.5
+  },
 
-let pokemon3 = {
-  name: 'Squirtle',
-  types: ['water'],
-  height: 0.5
-};
-
-let pokemon4 = {
-  name: 'Caterpie',
-  types: ['bug'],
-  height: 0.3
-};
-
-// Assign pokemon objects to our pokemonList
-pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4]
+  {
+    name: 'Caterpie',
+    types: ['bug'],
+    height: 0.3
+  }
+];
