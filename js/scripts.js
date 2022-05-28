7512e9cfdc03d2119b07a8b4c0e8d09c52c9cd89
xavsln <19362582+xavsln@users.noreply.github.com>
2022-05-28 18:29:16 +0700

    Wrap our pokemonList variable in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,47 @@
-// Create a new Array of Pokemon
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    types: ['grass', 'poison'],
-    height: 0.7
-  },
+// Wrap our pokemonList variable in an IIFE
+let pokemonRepository = (function () {
+  // Create a new Array of Pokemon
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      types: ['grass', 'poison'],
+      height: 0.7
+    },
 
-  {
-    name: 'Charmander',
-    types: ['fire'],
-    height: 0.6
-  },
+    {
+      name: 'Charmander',
+      types: ['fire'],
+      height: 0.6
+    },
 
-  {
-    name: 'Squirtle',
-    types: ['water'],
-    height: 0.5
-  },
+    {
+      name: 'Squirtle',
+      types: ['water'],
+      height: 0.5
+    },
 
-  {
-    name: 'Caterpie',
-    types: ['bug'],
-    height: 0.3
+    {
+      name: 'Caterpie',
+      types: ['bug'],
+      height: 0.3
+    }
+  ];
+
+  function getAll(pokemon){
+    return pokemonList;
   }
-];
 
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
+
+  // pokemonRepository function will return either getAll or add
+  return {
+    getAll: getAll, // if pokemonRepository.getAll() is selected then this will trigger the getAll(pokemon) function
+    add: add // if pokemonRepository.add() is selected then this will trigger the add(pokemon) function
+  }
+
+})();
 
 // Create a conditional that check the height of each Pokemon and indicate "Wow, that's big" in case it is higher than a certain value
 // Define a heigh that triggers the message
@@ -33,7 +50,7 @@ let thresholdHeight = 0.5;
 // Create a loop that compares all Pokemon height against the thresholdHeight
 // If the Pokemon height is higher than the defined thresholdHeight then "- Wow, that's big" message will be added to the name and height of the Pokemon
 // Otherwise, only the name and height of the Pokemon will be sent to the DOM
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
   if (pokemon.height > thresholdHeight) {
     document.write('<p>'+ pokemon.name + ' (heigh: '+ pokemon.height + ' m)' + ' - Wow, that\'s big!</p>');
   } else {
