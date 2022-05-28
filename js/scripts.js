d0acfcee2916a0588397b46272f866011a497d17
xavsln <19362582+xavsln@users.noreply.github.com>
2022-05-28 11:58:47 +0700

    Refactor the loop (forEach vs For) to show Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,6 @@ let pokemonList = [
   }
 ];
 
-// Iterate through the pokemonList to show each Pokemon name and height
-// for (let i = 0; i < pokemonList.length; i++) {
-//   document.write('<p>'+ pokemonList[i].name + ' (heigh: '+ pokemonList[i].height + ' m)' + '</p>');
-// }
 
 // Create a conditional that check the height of each Pokemon and indicate "Wow, that's big" in case it is higher than a certain value
 // Define a heigh that triggers the message
@@ -37,10 +33,10 @@ let thresholdHeight = 0.5;
 // Create a loop that compares all Pokemon height against the thresholdHeight
 // If the Pokemon height is higher than the defined thresholdHeight then "- Wow, that's big" message will be added to the name and height of the Pokemon
 // Otherwise, only the name and height of the Pokemon will be sent to the DOM
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > thresholdHeight) {
-    document.write('<p>'+ pokemonList[i].name + ' (heigh: '+ pokemonList[i].height + ' m)' + ' - Wow, that\'s big</p>');
+pokemonList.forEach(function(pokemon){
+  if (pokemon.height > thresholdHeight) {
+    document.write('<p>'+ pokemon.name + ' (heigh: '+ pokemon.height + ' m)' + ' - Wow, that\'s big!</p>');
   } else {
-    document.write('<p>'+ pokemonList[i].name + ' (heigh: '+ pokemonList[i].height + ' m)' + '</p>');
+    document.write('<p>'+ pokemon.name + ' (heigh: '+ pokemon.height + ' m)' + '</p>');
   }
-}
+});
